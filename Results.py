3b2e1c547109a02087e56a09157911f4a6ecb058
bobert <67173136+Shappybappy@users.noreply.github.com>
2022-04-20 15:48:18 -0500

    Changed variable names in results
    Changed variable names in results to be consistent with regular python naming

--- Results.py
+++ Results.py
@@ -6,18 +6,19 @@ from SpecifySearch import SEARCH_ITEM
 
 
 def results(post):
-    checkIfPrintedPostAmount = 0
-    for i, post in enumerate(totalPosts):
-        titleDiv = post.find('a', class_='result-title')
-        postTitle = titleDiv.get_text()
-        postURL = titleDiv.get('href')
-        postTimeText = post.find('time').get('datetime')
-        postTime = datetime.strptime(postTimeText, '%Y-%m-%d %H:%M')
-        ellaspedMins = (datetime.now() - postTime)
-        
-        if checkIfPrintedPostAmount < 1:
-            print(f'{len(totalPosts)} results containg "{SEARCH_ITEM}"')
-        print(f'{i+1}: {postTitle} -- {ellaspedMins} ++ {postURL} ||||')
+    check = 0
+    for i, post in enumerate(total_posts):
+        title_div = post.find('a', class_='result-title')
+        post_title = title_div.get_text()
+        post_URL = title_div.get('href')
+        post_time_text = post.find('time').get('datetime')
+        post_time = datetime.strptime(post_time_text, '%Y-%m-%d %H:%M')
+        ellasped_mins = (datetime.now() - post_time)
 
-totalPosts = stepThroughPages([], '/search/zip?')
-totalPosts = [post for post in totalPosts if SEARCH_ITEM in (post.find('a', class_='result-title').get_text()).lower()]
+        if check < 1:
+            print(f'{len(total_posts)} results containg "{SEARCH_ITEM}"')
+            check+=1
+        print(f'{i+1}: {post_title} -- {ellasped_mins} ++ {post_URL} ||||')
+
+total_posts = stepThroughPages([], '/search/zip?')
+total_posts = [post for post in total_posts if SEARCH_ITEM in (post.find('a', class_='result-title').get_text()).lower()]
