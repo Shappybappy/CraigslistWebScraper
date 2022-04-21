46fa4b1ced2b1064c973fb4d71cf7c86fc61e534
bobert <67173136+Shappybappy@users.noreply.github.com>
2022-04-20 19:41:57 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,22 +1,22 @@
-#GUI
+#GUI.py
 import tkinter as tk
-from SpecifySearch import searchSpec
+
 
 root = tk.Tk()
 root.title("Craigslist scraper")
-
 
 canvas = tk.Canvas(root, height=700, width=1200, bg="#263d42")
 
 frame1 = tk.Frame(root, bg="gray")
 frame1.place(relwidth=0.5, relheight=0.7, relx=0.25, rely=0.05)
 
-thing = tk.Entry(root, width=30)
+entryTK = tk.Entry(root, width=30)
+
 
 def myClick():
-    label= tk.Label(root, text=thing.get())
+    label= tk.Label(root, text=entryTK.get())
     label.pack()
-    searchSpec(thing.get())
+    return entryTK.get()
 
 button1 = tk.Button(frame1, text="button", command=myClick)
 button2 = tk.Button(frame1, text="done", command=root.destroy)
@@ -24,6 +24,6 @@ button2 = tk.Button(frame1, text="done", command=root.destroy)
 canvas.pack()
 button1.pack()
 button2.pack()
-thing.pack()
+entryTK.pack()
 
 root.mainloop()
