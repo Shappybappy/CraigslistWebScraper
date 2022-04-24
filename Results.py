e0fbebc81488c41f8a6956fc4b83b514183027f7
bobert <67173136+Shappybappy@users.noreply.github.com>
2022-04-24 10:31:14 -0500

    Made result.py work with updated version of program
    Made it so the program only now needs to use `stepThroughPages.py` and GUI.py

--- Results.py
+++ Results.py
@@ -2,11 +2,13 @@
 import datetime
 from datetime import datetime
 from StepThroughPages import stepThroughPages
-from SpecifySearch import SEARCH_ITEM
 
 
-def results(post):
+def results(SEARCH_ITEM):
     check = 0
+    total_posts = stepThroughPages([], '/search/zip?')
+    total_posts = [post for post in total_posts if SEARCH_ITEM in (post.find('a', class_='result-title').get_text()).lower()]
+
     for i, post in enumerate(total_posts):
         title_div = post.find('a', class_='result-title')
         post_title = title_div.get_text()
@@ -16,9 +18,10 @@ def results(post):
         ellasped_mins = (datetime.now() - post_time)
 
         if check < 1:
-            print(f'{len(total_posts)} results containg "{SEARCH_ITEM}"')
             check+=1
-        print(f'{i+1}: {post_title} -- {ellasped_mins} ++ {post_URL} ||||')
+            global actualResults
+            actualResults = f'{len(total_posts)} results containg "{SEARCH_ITEM}"\n'
 
-total_posts = stepThroughPages([], '/search/zip?')
-total_posts = [post for post in total_posts if SEARCH_ITEM in (post.find('a', class_='result-title').get_text()).lower()]
+        actualResults = actualResults + f'{i+1}: {post_title} -- {ellasped_mins} ++ {post_URL} ||||\n'
+
+    return actualResults
