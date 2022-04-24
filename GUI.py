2d87862f5dcf47e03b56b2508329df6ea8b3e2c1
bobert <67173136+Shappybappy@users.noreply.github.com>
2022-04-24 10:33:56 -0500

    Updated GUI.py
    made it so now all it needs is Results.py to work and it prints the results to the GUI window.
    I also changed some of the positionings of the entry, labels, and buttons

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 #GUI.py
-import time
 import tkinter as tk
+
+from Results import results
 
 
 root = tk.Tk()
@@ -11,26 +12,24 @@ canvas = tk.Canvas(root, height=700, width=1200, bg="#263d42")
 frame1 = tk.Frame(root, bg="gray")
 frame1.place(relwidth=0.5, relheight=0.7, relx=0.25, rely=0.05)
 
-entryTK = tk.Entry(root, width=30)
-
+entryTK = tk.Entry(frame1, width=30)
 
 def myClick():
-    label= tk.Label(root, text=entryTK.get())
-    label.pack()
-    return label.cget("text")
+    return results(entryTK.get())
+
+def displayOutput():
+    display_label = tk.Label(root, text=myClick()) 
+    display_label.pack()
 
 def close():
     root.destroy()
-    time.sleep(10)
-    print("closed")    
 
-button1 = tk.Button(frame1, text="button", command=myClick)
+button1 = tk.Button(frame1, text="Specify", command=displayOutput)
 button2 = tk.Button(frame1, text="done", command=close)
 
 canvas.pack()
 button1.pack()
-button2.pack()
 entryTK.pack()
+button2.pack()
 
 root.mainloop()
-
